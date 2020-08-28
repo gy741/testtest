0183efe2e34e7c8baad00a0db4beb79738c9c412
gy741 <gy741.kim@gmail.com>
2020-08-29 08:56:36 +0900

    Add files via upload

---
+++ dangerzone_project/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+import os
+import subprocess
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+#파일 업로드 처리
+@app.route('/fileUpload', methods = ['GET', 'POST'])
+def upload_file():
+   if request.method == 'POST':
+      f = request.files['file']
+      #저장할 경로 + 파일명
+      filename = f.filename
+      path = '/tmp/'
+      f.save(path + secure_filename(filename))
+      uploadpath = " " + path + filename + " "
+
+      subprocess.call(["/usr/bin/dangerzone-container" " documenttopixels --document-filename" + uploadpath + "--pixel-dir /tmp/dangerzone-pixel --container-name flmcode/dangerzone"], shell=True)
+      subprocess.call(["/usr/bin/dangerzone-container" " pixelstopdf --pixel-dir /tmp/dangerzone-pixel --safe-dir /tmp/dangerzone-safe --container-name flmcode/dangerzone --ocr 0 --ocr-lang eng"], shell=True)
+
+      os.rename("/tmp/dangerzone-safe/safe-output-compressed.pdf", "/tmp/dangerzone-safe/" + filename + "_" + "safe-output.pdf")
+      
+      return 'uploads 디렉토리 -> 파일 업로드 성공!'
+
+if __name__ == '__main__':
+    #서버 실행
+   app.run(host='0.0.0.0',port=5000,debug = True)
